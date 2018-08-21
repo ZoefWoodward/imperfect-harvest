18f3763b4ee916f07bd799a2d685d7b0b3d0b597
Zoe Woodward <zoefwoodward@gmail.com>
2018-08-21 14:30:10 -0400

    Update on Models/Migrations

---
+++ src/db/migrations/20180821154437-create-preferences.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Preferences', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "User",
+          key: "id",
+          as: "userId"
+        }
+      },
+      mixed: {
+        type: Sequelize.STRING
+      },
+      organic: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Preferences');
+  }
+};
